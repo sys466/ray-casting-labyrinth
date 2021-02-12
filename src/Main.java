37ef742ed816f880037ddd7cbca1929dd64d17d5
sys466 <sys466@outlook.com>
2021-02-12 21:41:07 +0300

    Removed deleted function call

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
     public static void main(String[] args) throws InvocationTargetException, InterruptedException {
 
-        ScreenRenderer.initMap();
         Runnable initGUI = GUI::new;
         SwingUtilities.invokeAndWait(initGUI);
 
