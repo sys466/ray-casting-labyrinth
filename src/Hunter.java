7830872ca5b17fb539431d4349a6a164de1ccd4d
sys466 <sys466@outlook.com>
2021-02-19 23:43:10 +0300

    Finished hunter class, ready for testing

--- src/Hunter.java
+++ src/Hunter.java
@@ -11,6 +11,8 @@ public class Hunter {
     private double positionX;
     private int direction;  // 0 - UP, 1 - DOWN, 2 - LEFT, 3 - RIGHT
     private double speed;
+    private int markerPositionY;
+    private int markerPositionX;
 
     public Hunter() {
         generatePosition();
@@ -20,16 +22,81 @@ public class Hunter {
 
     private void generatePosition() {
         do {
-            this.positionY = random.nextInt(MAP_HEIGHT - 1) + 1;
-            this.positionX = random.nextInt(MAP_WIDTH - 1) + 1;
-        } while (levelMap[(int) this.positionY][(int) this.positionX] != ' ');
+            this.markerPositionY = random.nextInt(MAP_HEIGHT - 1) + 1;
+            this.markerPositionX = random.nextInt(MAP_WIDTH - 1) + 1;
+        } while (levelMap[markerPositionY][markerPositionX] != ' ');
+        levelMap[markerPositionY][markerPositionX] = 'H';
+        this.positionY = markerPositionY;
+        this.positionX = markerPositionX;
     }
 
-    private void scan() {
-        boolean spotted = false;
+    private boolean scan() {
+        for (int i = 0; i < 4; i++) {
+            int positionY = (int) Math.floor(this.positionY);
+            int positionX = (int) Math.floor(this.positionX);
+            do {
+                switch (i) {
+                    case 0 -> positionY--;
+                    case 1 -> positionY++;
+                    case 2 -> positionX--;
+                    default -> positionX++;
+                }
+                if (levelMap[positionY][positionX] != 'P') {
+                    this.direction = i;
+                    return true;
+                }
+            } while (levelMap[positionY][positionX] != '#' && levelMap[positionY][positionX] != 'E');
+        }
+        return false;
+    }
+
+    private void changeDirection() {
+        int newDirection;
+        do {
+            newDirection = random.nextInt(4);
+        } while (newDirection == this.direction);
+        this.direction = newDirection;
+    }
+
+    private void checkMarker() {
+        if ((int) Math.floor(this.positionY) != this.markerPositionY
+                || (int) Math.floor(this.positionX) != this.markerPositionX) {
+            levelMap[this.markerPositionY][this.markerPositionX] = ' ';
+            this.markerPositionY = (int) Math.floor(this.positionY);
+            this.markerPositionX = (int) Math.floor(this.positionX);
+            levelMap[this.markerPositionY][this.markerPositionX] = 'H';
+        }
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public int getMarkerPositionY() {
+        return this.markerPositionY;
+    }
+
+    public int getMarkerPositionX() {
+        return this.markerPositionX;
     }
 
     public void move() {
+        int stepY = 0;
+        int stepX = 0;
+        switch (this.direction) {
+            case 0 -> stepY -= this.speed;
+            case 1 -> stepY += this.speed;
+            case 2 -> stepX -= this.speed;
+            default -> stepX += this.speed;
+        }
+        if (this.scan() && levelMap[(int) Math.floor(this.positionY + stepY)][(int) Math.floor(this.positionX + stepX)] != 'C'
+                || levelMap[(int) Math.floor(this.positionY + stepY)][(int) Math.floor(this.positionX + stepX)] == ' ') {
+            this.positionY += stepY;
+            this.positionX += stepX;
+            this.checkMarker();
+        } else {
+            changeDirection();
+        }
     }
 
 }
