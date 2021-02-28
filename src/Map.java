164a69f4ffcaad94c11fc7e26e86180fe7e3c95a
sys466 <sys466@outlook.com>
2021-02-28 16:25:28 +0300

    Moved some methods to proper classes

--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,8 @@
 import java.util.Random;
 
-public class Map {
+class Map {
 
-    private static final String level1MapData =
+    private static final String LEVEL_1_MAP_DATA =
         "########################" +
         "# #       # #   #      #" +
         "# # ##### #   # ###### #" +
@@ -28,7 +28,7 @@ public class Map {
         "#   #   #      #   #   #" +
         "########################";
 
-    private static final String level2MapData =
+    private static final String LEVEL_2_MAP_DATA =
         "########################" +
         "#  #  #     #   #   #  #" +
         "#  #      #   #   #   ##" +
@@ -54,77 +54,64 @@ public class Map {
         "#     #     #   #   #  #" +
         "########################";
 
-    private static final Random random = new Random();
-    private static final String[] MAPS = {level1MapData, level2MapData};
-    private static final int[] MAPS_HEIGHTS = {24, 24};
-    private static final int[] MAPS_WIDTHS = {24, 24};
-    private static final int MAP_NUMBER = random.nextInt(MAPS.length);
-    private static final int MAP_HEIGHT = MAPS_HEIGHTS[MAP_NUMBER];
-    private static final int MAP_WIDTH = MAPS_WIDTHS[MAP_NUMBER];
-    private static final int CARD_NUMBER = 3;
-    private static int unitPlayerPositionY;
-    private static int unitPlayerPositionX;
+    private static final Random RANDOM = new Random();
+    private static final String[] POSSIBLE_MAPS = {LEVEL_1_MAP_DATA, LEVEL_2_MAP_DATA};
+    private static final int[] POSSIBLE_MAPS_HEIGHTS = {24, 24};
+    private static final int[] POSSIBLE_MAPS_WIDTHS = {24, 24};
+    private static final int MAP_NUMBER = RANDOM.nextInt(POSSIBLE_MAPS.length);
+    private static final int MAP_HEIGHT = POSSIBLE_MAPS_HEIGHTS[MAP_NUMBER];
+    private static final int MAP_WIDTH = POSSIBLE_MAPS_WIDTHS[MAP_NUMBER];
+    private static final char[][] LEVEL_MAP = initMap();
 
-    public static char[][] initMap() {
-        char[][] levelMap = new char[MAPS_HEIGHTS[MAP_NUMBER]][MAPS_WIDTHS[MAP_NUMBER]];
+    private static char[][] initMap() {
+        char[][] levelMap = new char[MAP_HEIGHT][MAP_WIDTH];
         int levelMapDataIndex = 0;
-        for (int h = 0; h < MAPS_HEIGHTS[MAP_NUMBER]; h++) {
-            for (int w = 0; w < MAPS_WIDTHS[MAP_NUMBER]; w++) {
-                levelMap[h][w] = MAPS[MAP_NUMBER].charAt(levelMapDataIndex);
+        for (int h = 0; h < MAP_HEIGHT; h++) {
+            for (int w = 0; w < MAP_WIDTH; w++) {
+                levelMap[h][w] = POSSIBLE_MAPS[MAP_NUMBER].charAt(levelMapDataIndex);
                 levelMapDataIndex++;
             }
         }
-        generateUnitPlayer(levelMap);
-        generateCards(levelMap);
-        generateExit(levelMap);
         return levelMap;
     }
 
-    private static void generateUnitPlayer(char[][] levelMap) {
-        do {
-            unitPlayerPositionY = random.nextInt(MAP_HEIGHT - 1) + 1;
-            unitPlayerPositionX = random.nextInt(MAP_WIDTH - 1) + 1;
-        } while (levelMap[unitPlayerPositionY][unitPlayerPositionX] != ' ');
-    }
-
-    public static int getUnitPlayerPositionY() {
-        return unitPlayerPositionY;
-    }
-
-    public static int getUnitPlayerPositionX() {
-        return unitPlayerPositionX;
-    }
-
-    private static void generateCards(char[][] levelMap) {
-        int cardNumber = 0;
-        int cardPositionY;
-        int cardPositionX;
-        while (cardNumber < CARD_NUMBER) {
-            do {
-                cardPositionY = random.nextInt(MAP_HEIGHT - 1) + 1;
-                cardPositionX = random.nextInt(MAP_WIDTH - 1) + 1;
-            } while (levelMap[cardPositionY][cardPositionX] != ' ');
-            levelMap[cardPositionY][cardPositionX] = 'C';
-            cardNumber++;
-        }
-    }
-
-    private static void generateExit(char[][] levelMap) {
+    static {
         int exitPositionY;
         int exitPositionX;
         while (true) {
             do {
-                exitPositionY = random.nextInt(MAP_HEIGHT);
-                exitPositionX = random.nextInt(MAP_WIDTH);
-            } while (levelMap[exitPositionY][exitPositionX] != '#');
-            if (exitPositionY + 1 < MAP_HEIGHT && levelMap[exitPositionY + 1][exitPositionX] == ' '
-                    || exitPositionY - 1 >= 0 && levelMap[exitPositionY - 1][exitPositionX] == ' '
-                    || exitPositionX + 1 < MAP_WIDTH && levelMap[exitPositionY][exitPositionX + 1] == ' '
-                    || exitPositionX - 1 >= 0 && levelMap[exitPositionY][exitPositionX - 1] == ' ') {
-                levelMap[exitPositionY][exitPositionX] = 'E';
+                exitPositionY = RANDOM.nextInt(MAP_HEIGHT);
+                exitPositionX = RANDOM.nextInt(MAP_WIDTH);
+            } while (LEVEL_MAP[exitPositionY][exitPositionX] != '#');
+            if (exitPositionY + 1 < MAP_HEIGHT && LEVEL_MAP[exitPositionY + 1][exitPositionX] == ' '
+                    || exitPositionY - 1 >= 0 && LEVEL_MAP[exitPositionY - 1][exitPositionX] == ' '
+                    || exitPositionX + 1 < MAP_WIDTH && LEVEL_MAP[exitPositionY][exitPositionX + 1] == ' '
+                    || exitPositionX - 1 >= 0 && LEVEL_MAP[exitPositionY][exitPositionX - 1] == ' ') {
+                LEVEL_MAP[exitPositionY][exitPositionX] = 'E';
                 break;
             }
         }
+    }
+
+    public static boolean checkUnitPlayerPositionOnExit() {
+        final int UNIT_PLAYER_POSITION_Y = (int) Player.getUnitPlayerPositionY();
+        final int UNIT_PLAYER_POSITION_X = (int) Player.getUnitPlayerPositionX();
+        return LEVEL_MAP[UNIT_PLAYER_POSITION_Y + 1][UNIT_PLAYER_POSITION_X] == 'E'
+                || LEVEL_MAP[UNIT_PLAYER_POSITION_Y - 1][UNIT_PLAYER_POSITION_X] == 'E'
+                || LEVEL_MAP[UNIT_PLAYER_POSITION_Y][UNIT_PLAYER_POSITION_X + 1] == 'E'
+                || LEVEL_MAP[UNIT_PLAYER_POSITION_Y][UNIT_PLAYER_POSITION_X - 1] == 'E';
+    }
+
+    public static char[][] getLevelMap() {
+        return LEVEL_MAP;
+    }
+
+    public static int getMapHeight() {
+        return MAP_HEIGHT;
+    }
+
+    public static int getMapWidth() {
+        return MAP_WIDTH;
     }
 
 }
