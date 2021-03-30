30c225acfc13c381507fcd89f4245284d07a1448
sys466 <sys466@outlook.com>
2021-03-30 22:04:36 +0400

    Completely reworked hunter's algorithm

--- src/Hunter.java
+++ src/Hunter.java
@@ -9,19 +9,26 @@ class Hunter {
     private static final int MAP_HEIGHT = Map.getMapHeight();
     private static final int MAP_WIDTH = Map.getMapWidth();
     private static final double HUNTER_MAP_HALF_WIDTH = 0.1;
-    private static double speed = 0.02;
+    private static double speed = 0.01;
 
     private double positionY;
     private double positionX;
+    private int positionYOld;
+    private int positionXOld;
     private int direction;  // 0 - UP, 1 - DOWN, 2 - LEFT, 3 - RIGHT
+    private double stepY;
+    private double stepX;
+    private boolean found;
 
     static {
         hunters.add(new Hunter());
     }
 
     private Hunter() {
-        generatePosition();
+        this.generatePosition();
         this.direction = random.nextInt(4);
+        this.calculateSteps();
+        this.found = false;
     }
 
     public static void addHunter() {
@@ -35,16 +42,8 @@ class Hunter {
         } while (levelMap[(int) this.positionY][(int) this.positionX] != ' ');
         this.positionY += 0.5;
         this.positionX += 0.5;
-    }
-
-    public static boolean moveHunters() {
-        for (Hunter hunter: hunters) {
-            hunter.move();
-            if (hunter.checkUnitPlayerPosition()) {
-                return true;
-            }
-        }
-        return false;
+        this.positionYOld = (int) this.positionY;
+        this.positionXOld = (int) this.positionX;
     }
 
     public static boolean checkFOVVectorReachedHunter(double vectorPositionY, double vectorPositionX) {
@@ -57,56 +56,14 @@ class Hunter {
         return false;
     }
 
-    private void scan() {
-        for (int i = 0; i < 4; i++) {
-            int positionY = (int) this.positionY;
-            int positionX = (int) this.positionX;
-            do {
-                switch (i) {
-                    case 0 -> positionY--;
-                    case 1 -> positionY++;
-                    case 2 -> positionX--;
-                    default -> positionX++;
-                }
-                if (positionY == (int) Player.getUnitPlayerPositionY()
-                && positionX == (int) Player.getUnitPlayerPositionX()) {
-                    this.direction = i;
-                    return;
-                }
-            } while (levelMap[positionY][positionX] != '#' && levelMap[positionY][positionX] != 'E');
-        }
-    }
-
-    private void move() {
-        this.scan();
-        double stepY = 0;
-        double stepX = 0;
-        double centerY = 0;
-        double centerX = 0;
-        switch (this.direction) {
-            case 0 -> {
-                stepY = -speed;
-                centerY = -0.5;
-            }
-            case 1 -> {
-                stepY = speed;
-                centerY = 0.5;
-            }
-            case 2 -> {
-                stepX = -speed;
-                centerX = -0.5;
-            }
-            default -> {
-                stepX = speed;
-                centerX = 0.5;
+    public static boolean moveHunters() {
+        for (Hunter hunter: hunters) {
+            hunter.move();
+            if (hunter.checkUnitPlayerPosition()) {
+                return true;
             }
         }
-        if (levelMap[(int) (this.positionY + stepY + centerY)][(int) (this.positionX + stepX + centerX)] == ' ') {
-            this.positionY += stepY;
-            this.positionX += stepX;
-        } else {
-            changeDirection();
-        }
+        return false;
     }
 
     private boolean checkUnitPlayerPosition() {
@@ -114,12 +71,88 @@ class Hunter {
                 && (int) Math.floor(this.positionX) == (int) Math.floor(Player.getUnitPlayerPositionX());
     }
 
-    private void changeDirection() {
-        int newDirection;
-        do {
-            newDirection = random.nextInt(4);
-        } while (newDirection == this.direction);
-        this.direction = newDirection;
+    private void move() {
+        boolean spotted = this.hunt();
+        if (!this.found && ((int) this.positionY != this.positionYOld || (int) this.positionX != this.positionXOld)) {
+            this.navigate();
+        }
+        if (!spotted && levelMap[(int) (this.positionY + this.stepY)][(int) (this.positionX + this.stepX)] == '#') {
+            this.found = false;
+            this.navigate();
+        }
+        this.positionY += this.stepY;
+        this.positionX += this.stepX;
+    }
+
+    private boolean hunt() {
+        double playerPosY = Player.getUnitPlayerPositionY();
+        double playerPosX = Player.getUnitPlayerPositionX();
+        double difY = playerPosY - this.positionY;
+        double difX = playerPosX - this.positionX;
+        double length = Math.sqrt(Math.pow(difY, 2) + Math.pow(difX, 2));
+        double vectorStepY = difY / length;
+        double vectorStepX = difX / length;
+        double step = 0;
+        while ((int) (this.positionY + vectorStepY * step) != (int) playerPosY
+        || (int) (this.positionX + vectorStepX * step) != (int) playerPosX) {
+            if (levelMap[(int) (this.positionY + vectorStepY * step)][(int) (this.positionX + vectorStepX * step)] == '#') {
+                return false;
+            }
+            step += 0.05;
+        }
+        this.found = true;
+        this.stepY = vectorStepY * speed;
+        this.stepX = vectorStepX * speed;
+        return true;
+    }
+
+    private void navigate() {  // 0 - UP, 1 - DOWN, 2 - LEFT, 3 - RIGHT
+        ArrayList<Integer> paths = new ArrayList<>();
+        if (this.direction != 1 && levelMap[(int) this.positionY - 1][(int) this.positionX] != '#') {
+            paths.add(0);
+        }
+        if (this.direction != 0 && levelMap[(int) this.positionY + 1][(int) this.positionX] != '#') {
+            paths.add(1);
+        }
+        if (this.direction != 3 && levelMap[(int) this.positionY][(int) this.positionX - 1] != '#') {
+            paths.add(2);
+        }
+        if (this.direction != 2 && levelMap[(int) this.positionY][(int) this.positionX + 1] != '#') {
+            paths.add(3);
+        }
+        if (paths.isEmpty()) {
+            if (this.direction < 2) {
+                this.direction = this.direction == 0 ? 1 : 0;
+            } else {
+                this.direction = this.direction == 2 ? 3 : 2;
+            }
+        } else {
+            this.direction = paths.get(random.nextInt(paths.size()));
+        }
+        this.calculateSteps();
+        this.positionYOld = (int) this.positionY;
+        this.positionXOld = (int) this.positionX;
+    }
+
+    private void calculateSteps() {  // 0 - UP, 1 - DOWN, 2 - LEFT, 3 - RIGHT
+        switch (this.direction) {
+            case 0 -> {
+                this.stepY = -speed;
+                this.stepX = 0;
+            }
+            case 1 -> {
+                this.stepY = speed;
+                this.stepX = 0;
+            }
+            case 2 -> {
+                this.stepY = 0;
+                this.stepX = -speed;
+            }
+            default -> {
+                this.stepY = 0;
+                this.stepX = speed;
+            }
+        }
     }
 
     public static void increaseSpeed() {
